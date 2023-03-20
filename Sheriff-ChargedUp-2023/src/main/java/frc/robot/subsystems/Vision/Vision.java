19d8e5d58be46bec1d7653696a4ee5b0e2a5faf3
DoctorFogarty <john.fogarty@aol.com>
2023-03-20 15:14:50 -0400

    Testing more with Vision Pose EST

--- Sheriff-ChargedUp-2023/src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ Sheriff-ChargedUp-2023/src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -25,9 +26,9 @@ public class Vision extends SubsystemBase {
   private final Swerve swerve;
 
   //increase values to trust the drivetrain less (x,y,theta)
-  private static final Vector<N3> stateStdDevs = VecBuilder.fill(0.05,0.05, Units.degreesToRadians(0.01));
+  private static final Vector<N3> stateStdDevs = VecBuilder.fill(0.1,0.1, Units.degreesToRadians(0.1));
 
-  private static final Vector<N3> visionStdDevs = VecBuilder.fill(0.3,0.3, Units.degreesToRadians(500));
+  private static final Vector<N3> visionStdDevs = VecBuilder.fill(0.9,0.9, Units.degreesToRadians(0.9));
   //Values tried for X/Y 0.1, 1, 3, 5, 10
   //Values tried for Theta 0.1, 500 
   //(I think we want to ignore the camera pose's angles since they will always be wrong compared to gyro)
@@ -59,44 +60,48 @@ public class Vision extends SubsystemBase {
 
   @Override
   public void periodic() {
-    double leftTimestamp = LimelightHelpers.getLatestResults(Constants.Limelight.left).targetingResults.timestamp_LIMELIGHT_publish;
-    double rightTimestamp = LimelightHelpers.getLatestResults(Constants.Limelight.right).targetingResults.timestamp_LIMELIGHT_publish;
-
     // This method will be called once per scheduler run
 
     if(DriverStation.getAlliance()==Alliance.Blue){
-        var blueLeftBotPose = LimelightHelpers.getBotPose3d_wpiBlue(Constants.Limelight.left).toPose2d();
-        var blueRightBotPose = LimelightHelpers.getBotPose3d_wpiBlue(Constants.Limelight.right).toPose2d();
+        Pose2d blueLeftBotPose = LimelightHelpers.getBotPose2d_wpiBlue(Constants.Limelight.left);
+        Pose2d blueRightBotPose = LimelightHelpers.getBotPose2d_wpiBlue(Constants.Limelight.right);
 
-        if(isInMap(blueLeftBotPose) && isValidPose(blueLeftBotPose)){
-          leftCam.setRobotPose(blueLeftBotPose);
-          poseEst.addVisionMeasurement(blueLeftBotPose, leftTimestamp);
-        //swerve.resetOdometry(blueLeftBotPose);
+        Pose2d blueLeftBotNoRot = new Pose2d(new Translation2d(blueLeftBotPose.getX(), blueLeftBotPose.getY()),
+        swerve.getYaw());
+
+        Pose2d blueRightBotNoRot = new Pose2d(new Translation2d(blueRightBotPose.getX(), blueRightBotPose.getY()),
+        swerve.getYaw());
+
+        if(isInMap(blueLeftBotNoRot) && isValidPose(blueLeftBotNoRot)){
+          leftCam.setRobotPose(blueLeftBotNoRot);
+          poseEst.addVisionMeasurement(blueLeftBotNoRot, LimelightHelpers.getLatency_Pipeline(Constants.Limelight.left));
+          SmartDashboard.putNumber("Left Latancy", LimelightHelpers.getLatency_Pipeline(Constants.Limelight.left));
         }
         
-        if(isInMap(blueRightBotPose) && isValidPose(blueRightBotPose)){
-          rightCam.setRobotPose(blueRightBotPose);
-          poseEst.addVisionMeasurement(blueRightBotPose, rightTimestamp);
-        //swerve.resetOdometry(blueRightBosePose);
+        if(isInMap(blueRightBotNoRot) && isValidPose(blueRightBotNoRot)){
+          rightCam.setRobotPose(blueRightBotNoRot);
+          poseEst.addVisionMeasurement(blueRightBotNoRot,LimelightHelpers.getLatency_Pipeline(Constants.Limelight.right));
+          SmartDashboard.putNumber("Right Latency", LimelightHelpers.getLatency_Pipeline(Constants.Limelight.right));
         }
     }
 
-    if(DriverStation.getAlliance()==Alliance.Red){
-        var redLeftBotPose = LimelightHelpers.getBotPose2d_wpiRed(Constants.Limelight.left);
-        var redRightBotPose = LimelightHelpers.getBotPose2d_wpiRed(Constants.Limelight.right);
+    // if(DriverStation.getAlliance()==Alliance.Red){
+    //     var redLeftBotPose = LimelightHelpers.getBotPose2d_wpiRed(Constants.Limelight.left);
+    //     var redRightBotPose = LimelightHelpers.getBotPose2d_wpiRed(Constants.Limelight.right);
 
-        leftCam.setRobotPose(redLeftBotPose);
-        rightCam.setRobotPose(redRightBotPose);
+    //     leftCam.setRobotPose(redLeftBotPose);
+    //     rightCam.setRobotPose(redRightBotPose);
 
 
-        poseEst.addVisionMeasurement(redLeftBotPose, leftTimestamp);
-        //swerve.resetOdometry(redLeftBotPose);
+    //     poseEst.addVisionMeasurement(redLeftBotPose, LimelightHelpers.getLatency_Capture(Constants.Limelight.left));
+    //     //swerve.resetOdometry(redLeftBotPose);
 
-        poseEst.addVisionMeasurement(redRightBotPose, rightTimestamp);
-        //swerve.resetOdometry(redRightBotPose);
-    }
+    //     poseEst.addVisionMeasurement(redRightBotPose, LimelightHelpers.getLatency_Capture(Constants.Limelight.right));
+    //     //swerve.resetOdometry(redRightBotPose);
+    // }
 
     poseEst.update(swerve.getYaw(), swerve.getModulePositions());
+
     field2d.setRobotPose(getCurrentPose());
 
     SmartDashboard.putData("Field Pose Est", field2d);
